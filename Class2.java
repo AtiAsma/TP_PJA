c443cedb5f14f74e493d4f31d808c59bc6cc67d9
Asma Ati <58991297+AtiAsma@users.noreply.github.com>
2021-11-10 22:42:16 +0100

    Update Class2.java

--- Class2.java
+++ Class2.java
@@ -13,13 +13,13 @@ public class Class2 {
        
         try {
             ObjectInputStream obj = new ObjectInputStream(new FileInputStream("emp.dat"));
-            RandomAccessFile raf2 = new RandomAccessFile("emp.dat", "r");
+            /*RandomAccessFile raf2 = new RandomAccessFile("emp.dat", "r");*/
             RandomAccessFile raf = new RandomAccessFile("empdirect.dat", "rw");
         
             while (raf.getFilePointer() < raf.length()) {
                  
                  em = (Employee) obj.readObject();
-                 System.out.println(raf2.readUTF());
+                /* System.out.println(raf2.readUTF());*/
                  raf.writeUTF(em.name);
                  raf.writeUTF(em.address);
                  raf.writeUTF(String.valueOf(em.SSN));
